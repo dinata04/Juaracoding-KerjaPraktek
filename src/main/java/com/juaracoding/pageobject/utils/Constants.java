e4520d32cf39ffc0ceae4a6b3c9a41426ab82e72
dinata04 <hendyhadynata@gmail.com>
2022-09-09 23:14:32 +0700

    framework

--- src/main/java/com/juaracoding/pageobject/utils/Constants.java
+++ src/main/java/com/juaracoding/pageobject/utils/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	public static final String CHROME = "Chrome";
 	public static final String FIREFOX = "Firefox";
 	public static final String URLOne = "https://dev.ptdika.com/web_jc_v2/admin/login";
-	public static final String URLTwo = "https://dev.ptdika.com/web_jc_v2/";
+	
 	
 }
 	
